b5d72eac67fad66e2806dda18f11f8c8310902c6
prepat21 <88353973+prepat21@users.noreply.github.com>
2023-01-09 15:49:49 -0600

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ const cache = new InMemoryCache({
 });
 
 const client = new ApolloClient({
-  uri: "https://fatigue-mgmt-system-api.onrender.com/",
+  uri: "http://localhost:5000/graphql",
   cache,
 });
 
