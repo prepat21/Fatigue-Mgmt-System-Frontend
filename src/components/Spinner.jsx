803bf3cfc5f45b6e0a9f57072d1ba10bf740fbb5
prepat21 <88353973+prepat21@users.noreply.github.com>
2023-01-24 20:07:39 -0600

    Delete src directory

--- src/components/Spinner.jsx
+++
@@ -1,9 +0,0 @@
-export default function Spinner() {
-  return (
-    <div className="d-flex justify-content-sm-center">
-      <div className="spinner-border" role="status">
-        <span className="sr-only"></span>
-      </div>
-    </div>
-  );
-}
